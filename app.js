4afccecc86d8aea97da5e8e9cc80059a0a6c0c11
shahjalalcse <shah-jalal1>
2021-02-09 20:39:09 +0600

    mp3 add

--- app.js
+++ app.js
@@ -12,17 +12,25 @@ const displaySongs = songs => {
     // songs.forEach(song => console.log(song.title));
 
     songs.forEach(song => {
+        // console.log(song);
         const songDiv = document.createElement('div');
         songDiv.className = 'single-result row align-items-center my-3 p-3';
         songDiv.innerHTML = `
         <div class="col-md-9">
                         <h3 class="lyrics-name">${song.title}</h3>
                         <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                        <audio controls>
+                            <source src="${song.preview}" type="audio/mpeg">
+                        </audio> 
         </div>
         <div class="col-md-3 text-md-right text-center">
-                        <button class="btn btn-success">Get Lyrics</button>
+                 <button onclick="getLyric('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button> 
         </div>
         `;
         songContainer.appendChild(songDiv);
     });
+}
+
+const getLyric = (artist, title) => {
+    console.log(artist, title);
 }
